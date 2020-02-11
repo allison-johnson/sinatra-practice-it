24f3277cd3222fd2c462cd6a1ebfecddec9a02da
allison_johnson <allison.cuttler@gmail.com>
2020-02-11 12:33:33 -0800

    gave teacher model has many topics through questions

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,6 +2,7 @@ class Teacher < ActiveRecord::Base
   has_secure_password #Includes presence validator on password, do NOT add another one
   has_many :students
   has_many :questions, through: :students
+  has_many :topics, through: :questions
 
   #validates :username, with: /^[A-Za-z0-9]+$/
   validates_presence_of :first_name, :last_name, :username
