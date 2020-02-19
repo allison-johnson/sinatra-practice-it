4c1be44a933a7c947f2b740aacd16b6963690fa0
allison_johnson <allison.cuttler@gmail.com>
2020-02-18 16:01:23 -0800

    DRYed out student update action by calling helper methods and utilzing validations

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -43,6 +43,7 @@ class StudentsController < ApplicationController
     if !set_student
       redirect '/failure'
     elsif authorized?(@student.teacher)
+      @field_values = {}
       erb :'students/edit'
     else
       redirect '/failure'
@@ -58,52 +59,36 @@ class StudentsController < ApplicationController
       @field_values = params
       erb :'students/new'
     else
-      set_teacher(session)
       @teacher.students << student 
       @teacher.save 
       redirect "/teachers/#{@teacher.id}"
     end #if
   end #action
 
-  # post '/students' do
-  #   if params[:first_name] == "" || params[:last_name] == "" || params[:grade] == "" 
-  #     puts "empty field"
-  #     flash[:message] = "Whoops - looks like you forgot to complete a field!"
-  #     #binding.pry 
-  #     redirect '/students/new'
-    
-  #   #active record where method 
-  #   elsif Student.all.map{|student| "#{student.first_name} #{student.last_name}"}.include?("#{params[:first_name]} #{params[:last_name]}")
-  #     flash[:message] = "Looks like that student is already in our database!"
-  #     redirect '/students/new'
-
-  #   else #If nothing was blank and student is not already in DB attempt to create a new Student...
-  #     student = Student.new(first_name: params[:first_name], last_name: params[:last_name], grade: params[:grade].to_i)
-  #     #binding.pry 
-  #     if !student.save #Some validation failed, back /students/new
-  #       flash[:message] = "#{student.errors.messages.keys.first.to_s} #{student.errors.messages.values.first[0]}"
-  #       redirect '/students/new'
-  #     else #All validations passed, redirect to teacher show page
-  #       @teacher = Teacher.find_by(id: session[:id])
-  #       @teacher.students << student 
-  #       @teacher.save 
-  #       redirect "/teachers/#{@teacher.id}"
-  #     end #if able to save
-  #   end #if
-  # end #action
-
-  #update action 
+  #update action
   patch '/students/:id' do
-    @student = Student.find_by(id: params[:id])
-    if params[:first_name] == "" || params[:last_name] == "" || params[:grade] == ""
-      redirect "/students/#{params[:id]}/edit"
+    set_student
+    if !@student.update(first_name: params[:first_name], last_name: params[:last_name], grade: params[:grade])
+      flash[:message] = @student.errors.full_messages.join(", ")
+      @field_values = params
+      erb :'students/edit'
     else
-      @student.update(first_name: params[:first_name])
-      @student.update(last_name: params[:last_name])
-      @student.update(grade: params[:grade])
-      redirect "/teachers/#{session[:id]}"
+      redirect "/students/#{@student.id}"
     end #if
   end #action
+
+  #update action 
+  # patch '/students/:id' do
+  #   @student = Student.find_by(id: params[:id])
+  #   if params[:first_name] == "" || params[:last_name] == "" || params[:grade] == ""
+  #     redirect "/students/#{params[:id]}/edit"
+  #   else
+  #     @student.update(first_name: params[:first_name])
+  #     @student.update(last_name: params[:last_name])
+  #     @student.update(grade: params[:grade])
+  #     redirect "/teachers/#{session[:id]}"
+  #   end #if
+  # end #action
 
   #update students' completed questions
   patch '/students/:id/update-questions' do
